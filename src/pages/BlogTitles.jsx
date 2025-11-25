fc089f3411486226b94501951089b2c51d456877
shabbirmahmud7 <shabbirmahmud.sm7.b@gmail.com>
2025-11-25 18:51:44 +0600

    front end done
    all new changes

--- src/pages/BlogTitles.jsx
+++ src/pages/BlogTitles.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Sparkles, Hash} from 'lucide-react'
 
 const BlogTitles = () => {
+
+  const blogCatagories = ['General', 'Technology', 'Business' , 'Health' ,
+    'Lifestyle' , 'Education', 'Travel' , 'Food' ]
+
+  const [selectedCategory, setSelectedCategory] = useState('General')
+  
+
   return (
-    <div>
-        <h1>BlogTitles</h1> 
+  
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+       {/* l.c */}
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+       <div className='flex item-center gap-3'>
+        <Sparkles className='w-6 text-[#ff4aff]' />
+        <h1 className='text-xl font-semibold'>AI Title Generator</h1>
+       </div>
+       <p className='mt-6 text-sm font-medium'>Keyword</p>
+      <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='The future of artifitial intelligence is....' required/>
+      <p className='mt-4 text-sm font-medium'>Catagory</p>
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {blogCatagories.map((item)=>(
+            <span onClick={()=> setSelectedCategory(item)} 
+            className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedCategory === item ? 'bg-blue-50 text-blue-700' : 'text-gray-500 border-gray-300'} `} key={item}>{item}</span>
+          ))}
+        </div>
+        <br />
+          <button className='w-full flex justify-center item-center gap-2 bg-gradient-to-r from-[#35f804] to-[#f32905] text-black px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <Hash className='w-5'/>
+          Generate title
+          </button>
+      </form >
+     {/* R.c */}
+     
+     <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 '>
+          <div className='flex item-center gap-3'>
+            <Hash className='w-5 h-5 text-[#4A7AFF]'/>
+              <h1 className='text-xl font-semibold'>Generated titles</h1>
+           </div>
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5 text-gray-400' >
+            <Hash className='w-9 h-9'/>
+            <p>Enter a topic and click "Generate title" to get started</p>
+            </div>
+          </div> 
+          
+          </div>
+    
+
     </div>
+
+
   )
 }
 
--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Sparkles,Edit} from 'lucide-react'
 
 const WriteArticle = () => {
+  const articleLength = [
+    {length: 800, text:'Short (500-800 words)'},
+    {length: 1200, text:'Medium (800-1200 words)'},
+    {length: 1600, text:'Long (1200+ words)'},
+  ]
+
+  
+    
+
+  const [selectedLength, setSelectedLength] = useState(articleLength[0])
+  const [input, setInput] = useState('')
+
+  const onSubmitHandler = async (e)=>{
+        e.preventDefault();
+  }
+
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
-       
+       {/* l.c */}
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+       <div className='flex item-center gap-3'>
+        <Sparkles className='w-6 text-[#4A7AFF]' />
+        <h1 className='text-xl font-semibold'>Article Configuration</h1>
+       </div>
+       <p className='mt-6 text-sm font-medium'>Article Topic</p>
+      <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='The future of artifitial intelligence is....' required/>
+      <p className='mt-4 text-sm font-medium'>Article Length</p>
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {articleLength.map((item, index)=>(
+            <span onClick={()=> setSelectedLength(item)} 
+            className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedLength.text === item.text ? 'bg-red-50 text-red-700' : 'text-gray-500 border-gray-300'} `} key={index}>{item.text}</span>
+          ))}
+        </div>
+        <br />
+          <button className='w-full flex justify-center item-center gap-2 bg-gradient-to-r from-[#f8bf04] to-[#65ADFF] text-black px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <Edit className='w-5'/>
+          Generate article
+          </button>
+      </form >
+     {/* R.c */}
+     
+     <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px] '>
+          <div className='flex item-center gap-3'>
+            <Edit className='w-5 h-5 text-[#4A7AFF]'/>
+              <h1 className='text-xl font-semibold'>Generated article</h1>
+           </div>
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5 text-gray-400' >
+            <Edit className='w-9 h-9'/>
+            <p>Enter a topic click "Generate article" to get start</p>
+            </div>
+          </div> 
+          
+          </div>
+    
+
     </div>
   )
 }
